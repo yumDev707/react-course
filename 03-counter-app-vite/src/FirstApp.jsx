69fc375ee296dfabab0f513ae33af79494e3c686
Jennifer D. <jennifer.diezs@gmail.com>
2024-09-15 23:56:36 +0200

    Sections 34-38. First app with React.

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,11 @@
+// import {Fragment} from 'react';
+
+export const FirstApp = () => {
+    return (
+        //Fragmento implícito, siempre necesario. De esta forma no necesitamos importarlo.
+        <>
+            <div>My first app using React. :)</div>
+            <p>Yay!</p>
+        </>
+    );
+}
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDom from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {HelloWorldApp} from './HelloWorldApp';
+import {FirstApp} from './FirstApp';
+
+ReactDom.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <HelloWorldApp />
+        <FirstApp />
+    </React.StrictMode>
+);
