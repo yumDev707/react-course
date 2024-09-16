5b72117d4b914152131a55291d315283d660e97c
Jennifer D. <jennifer.diezs@gmail.com>
2024-09-16 18:58:21 +0200

    Sections 38-44.

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,11 +1,36 @@
 // import {Fragment} from 'react';
+import PropTypes from 'prop-types';
 
-export const FirstApp = () => {
+const getGretting = () => {
+    return 'Hey! I am a function!'
+}
+
+const newMsg = {
+    title: 'Prueba de variable en HTML.', 
+    main: 'Jenn'
+}
+
+export const FirstApp = ({title, subtitle, name}) => {
     return (
         //Fragmento implícito, siempre necesario. De esta forma no necesitamos importarlo.
         <>
             <div>My first app using React. :)</div>
             <p>Yay!</p>
+            <p>{JSON.stringify(newMsg)}</p>
+            <p>{getGretting()}</p>
+            <p>{title}</p>
+            <p>{subtitle}</p>
+            <p>{name}</p>
         </>
     );
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.number
+}
+
+FirstApp.defaultProps = {
+    subtitle: 'No subtitle... and this is a string!',
+    name: 'Jeeeenn'
 }
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -3,10 +3,15 @@ import ReactDom from 'react-dom/client';
 
 import {HelloWorldApp} from './HelloWorldApp';
 import {FirstApp} from './FirstApp';
+import {CounterApp} from './CounterApp';
+import './styles.css';
 
 ReactDom.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <HelloWorldApp />
-        <FirstApp />
+        {/* De esta forma enviamos prop (properties). El valor subtitle envía núms enteros. */}
+        {/* <FirstApp title="It is Yum!" subtitle={123}/> */}
+        <FirstApp title='Required title'/>
+        <CounterApp value={3}/>
     </React.StrictMode>
 );
