788435935f34bb34d87d4873dea7b7df1f00dc9f
Jennifer D. <jennifer.diezs@gmail.com>
2024-10-01 20:57:53 +0200

    Sections 45-48.

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,14 +1,36 @@
-import PropTypes from 'prop-types';
+import PropTypes, { func } from 'prop-types';
+import {useState} from 'react';
 
 export const CounterApp = ({value}) => {
+    const [counter, setCounter] = useState(value); //Hook. Desestructuración de lo que retorna la función, que inicializamos en 0.
+    
+    const handleAdd = (event, string) => { 
+        // console.log(string, '+1');
+        setCounter(counter + 1);
+    }
+
+    const handleLess = () => setCounter(counter - 1);
+    const handleReset = () => setCounter(value);
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            {/* <h2>{value}</h2> */}
+            <h2>{counter}</h2>
+
+            <button onClick={(event) => handleAdd(event, 'Hey!')}>
+                Click me!
+            </button>
+
+            <div class="button-container">
+                <button onClick={(event) => handleAdd(event, 'Hey!')}>+1</button>
+                <button onClick={handleLess}>-1</button>
+                <button onClick={handleReset}>Reset</button>
+            </div>
         </>
     )
 }
 
-PropTypes.propTypes = {
+CounterApp.propTypes = {
     value: PropTypes.number
 }
